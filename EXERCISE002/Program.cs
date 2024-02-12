83804bbbc72267f1136bf9019aa7023b4644028b
TecNM <TecNM@DESKTOP-BABKH5A>
2024-02-12 16:40:22 -0600

    Agregar archivos de proyecto.

---
+++ EXERCISE002/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Num, M;
+Console.WriteLine("Write a number");
+Num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < 11; i++)
+{
+    M = Num * i;
+    Console.WriteLine(Num + "X" + i + "=" + M);
+}
+Console.ReadKey();
